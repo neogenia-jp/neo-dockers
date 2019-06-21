9fd0c673ed08a7a72fd15ff4aaf1fc485e517350
Wataru Maeda <w.maeda@neogenia.co.jp>
2019-06-21 13:01:00 +0900

    ruby2.6 のDocker構成を追加。jemallocを導入してみました。

---
+++ ruby2.6/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Usage:
+#   ./build.sh <ruby_version> [tag]
+# Example:
+#   ./build.sh 2.6.3 latest
+#
+SCRIPT_DIR=${SCRIPT_DIR:-$(cd $(dirname $0) && pwd)}
+
+RUBY_VERSION=${1:-2.6.3}
+TAG=${2:-$RUBY_VERSION}
+
+IMAGE_NAME=neogenia/$(basename $SCRIPT_DIR)
+NAME_TAG=$IMAGE_NAME:$TAG
+echo building image "$NAME_TAG" ...
+
+(cd $SCRIPT_DIR; time docker build -t $NAME_TAG --build-arg RUBY_VERSION=$RUBY_VERSION .) \
+&& cat <<GUIDE
+# build finished successfuly.
+# If you push image to DockerHub, use below command:
+
+docker login
+
+docker push $NAME_TAG
+
+GUIDE
