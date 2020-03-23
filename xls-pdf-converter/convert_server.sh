aa763c90903e894b121eac538636a04b43409537
Wataru Maeda <w.maeda@neogenia.co.jp>
2020-03-23 19:19:07 +0900

    feat: Excel PDF 変換の簡易版デーモンにてFIFOを使うよう修正

--- xls-pdf-converter/convert_server.sh
+++ xls-pdf-converter/convert_server.sh
@@ -7,6 +7,10 @@ echo "nc 127.0.0.1 $LISTEN_PORT < /path/to/input.xls > /path/to/output.pdf"
 echo ""
 echo "Listening port $LISTEN_PORT ..."
 
+FIFO=/tmp/convert.io
+mkfifo $FIFO
+tail -f $FIFO &
+
 while :; do
-  nc -klp $LISTEN_PORT -e bash -c 'cat > /tmp/aa && ./convert.rb /tmp/aa /tmp/ && cat /tmp/aa.pdf';
+  nc -klp $LISTEN_PORT -e bash -c "cat > /tmp/aa && ./convert.rb /tmp/aa /tmp/ > $FIFO 2>&1 && cat /tmp/aa.pdf";
 done
