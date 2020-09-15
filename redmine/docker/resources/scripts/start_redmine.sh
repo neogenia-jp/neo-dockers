0e3ac146f8ef307a8e65ce68d7fab78be3bb29d2
Wataru Maeda <w.maeda@neogenia.co.jp>
2020-09-15 18:48:50 +0900

    feat: upgrade Redmine 4.1

--- redmine/docker/resources/scripts/start_redmine.sh
+++ redmine/docker/resources/scripts/start_redmine.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+LOCK_FILE=/var/lib/mysql/.db_initialized
+
+export RAILS_ENV=production
+
 cd /usr/share/redmine
 
-if [ ! -f .db_initialized ]; then
+if [ ! -f $LOCK_FILE ]; then
   echo '----- INITIALIZE DB -----'
 
   # Exec database initialize SQL script.
@@ -13,7 +17,7 @@ if [ ! -f .db_initialized ]; then
     echo '----- importing old_data -----'
     zcat /mnt/mysql_data.sql.gz | mysql redmine
   fi
-  touch .db_initialized
+  touch $LOCK_FILE
 fi
 
 # PIDファイルを削除
@@ -22,9 +26,12 @@ rm tmp/pids/* 2>/dev/null
 LOG_FILE=log/production.log
 
 echo '----- bundle install -----'
+gem install mysql2 -v '0.5.3' --source 'https://rubygems.org/'  # patch for mysql2 gem install error
+gem install puma -v '3.12.6' --source 'https://rubygems.org/'
 bundle install
 
 echo '----- database migrating -----'
+bin/rake generate_secret_token
 bin/rake db:migrate
 
 chronic bin/rails server -b 0.0.0.0 -d \
