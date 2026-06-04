10ef50f73516068a6ba58272ad9f3fb53e183333
TomoProg <t.yamamoto@neogenia.co.jp>
2026-06-04 14:56:28 +0900

    fix: pushオプションは使わないように元に戻した

--- rails-basic/build.sh
+++ rails-basic/build.sh
@@ -2,42 +2,20 @@
 #
 # Usage:
 #   ./build.sh <ruby_version> [--push] [tag]
-#   --pushオプションはbuildxを使用している場合にのみ有効で、ビルド後にイメージをDockerHubにプッシュします。
 #   tagは省略した場合はruby_versionと同じになります。
 # Example:
 #   ./build.sh 2.7.0
-#   ./build.sh 2.7.0 --push
 #   ./build.sh 2.7.0 latest
-#   ./build.sh 2.7.0 --push latest
 #
 SCRIPT_DIR=${SCRIPT_DIR:-$(cd $(dirname $0) && pwd)}
 
 RUBY_VERSION=${1:-2.7.0}
+TAG=${2:-$RUBY_VERSION}
 
 # buildxを使えるか判定する
 ENABLE_BUILD_X=""
 if docker buildx version >/dev/null 2>&1; then
   ENABLE_BUILD_X=true
-fi
-
-# 第1引数（Rubyバージョン）以降のコマンド引数を確認する
-# pushオプション以外はタグとして扱う。
-shift || true
-PUSH_FLAG=""
-TAG=""
-for arg in "$@"; do
-  if [[ "$arg" == "--push" && $ENABLE_BUILD_X ]]; then
-    PUSH_FLAG="--push"
-  else
-    if [[ -z "$TAG" ]]; then
-      TAG="$arg"
-    fi
-  fi
-done
-
-# タグが指定されない場合はRubyバージョンをタグにする。
-if [[ -z "$TAG" ]]; then
-  TAG="$RUBY_VERSION"
 fi
 
 IMAGE_NAME=neogenia/$(basename $SCRIPT_DIR)
@@ -46,22 +24,19 @@ echo building image "$NAME_TAG" ...
 
 if [[ -n "$ENABLE_BUILD_X" ]]; then
   echo "Using buildx (multi-arch build)"
-  DOCKER_BUILD_COMMAND_BASE="docker buildx build --platform linux/amd64,linux/arm64 $PUSH_FLAG"
+  DOCKER_BUILD_COMMAND_BASE="docker buildx build --platform linux/amd64,linux/arm64"
 else
   echo "Warning: buildx not found. Unable to multi-arch build"
   DOCKER_BUILD_COMMAND_BASE="docker build"
 fi
 
 (cd $SCRIPT_DIR; time $DOCKER_BUILD_COMMAND_BASE -t $NAME_TAG --build-arg RUBY_VERSION=$RUBY_VERSION  .) \
-&& echo "# build finished successfuly."
-
-# 自動pushしない場合はpushコマンドの案内を表示する
-if [[ -z "$PUSH_FLAG" ]]; then
-  cat <<GUIDE
+&& cat <<GUIDE
+# build finished successfuly.
 # If you push image to DockerHub, use below command:
 
 docker login
 
 docker push $NAME_TAG
+
 GUIDE
-fi
--- ruby/build.sh
+++ ruby/build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 #
 # Usage:
-#   ./build.sh <ruby_version> [--push] [build_options...]
-#   --pushオプションはbuildxを使用している場合にのみ有効で、ビルド後にイメージをDockerHubにプッシュします。
+#   ./build.sh <ruby_version> [build_options...]
 # Example:
 #   ./build.sh 2.7.1 --with-jemalloc
-#   ./build.sh 2.7.1 --push --with-jemalloc
 #
 SCRIPT_DIR=${SCRIPT_DIR:-$(cd $(dirname $0) && pwd)}
 
@@ -18,44 +16,25 @@ if docker buildx version >/dev/null 2>&1; then
   ENABLE_BUILD_X=true
 fi
 
-# 第1引数（Rubyバージョン）以降のコマンド引数を確認する
-shift || true
-PUSH_FLAG=""
-BUILD_OPTS=""
-for arg in "$@"; do
-  if [[ "$arg" == "--push" && $ENABLE_BUILD_X ]]; then
-    PUSH_FLAG="--push"
-  else
-    if [[ -z "$BUILD_OPTS" ]]; then
-      BUILD_OPTS="$arg"
-    else
-      BUILD_OPTS="$BUILD_OPTS $arg"
-    fi
-  fi
-done
-
 IMAGE_NAME=neogenia/ruby
 NAME_TAG=$IMAGE_NAME:$TAG
 echo building image "$NAME_TAG" ...
 
 if [[ -n "$ENABLE_BUILD_X" ]]; then
   echo "Using buildx (multi-arch build)"
-  DOCKER_BUILD_COMMAND_BASE="docker buildx build --platform linux/amd64,linux/arm64 $PUSH_FLAG"
+  DOCKER_BUILD_COMMAND_BASE="docker buildx build --platform linux/amd64,linux/arm64"
 else
   echo "Warning: buildx not found. Unable to multi-arch build"
   DOCKER_BUILD_COMMAND_BASE="docker build"
 fi
 
-(cd $SCRIPT_DIR; time $DOCKER_BUILD_COMMAND_BASE -t $NAME_TAG --build-arg RUBY_VERSION=$RUBY_VERSION --build-arg BUILD_OPTS="$BUILD_OPTS" .) \
-&& echo "# build finished successfuly."
-
-# 自動pushしない場合はpushコマンドの案内を表示する
-if [[ -z "$PUSH_FLAG" ]]; then
-  cat <<GUIDE
+(cd $SCRIPT_DIR; time $DOCKER_BUILD_COMMAND_BASE -t $NAME_TAG --build-arg RUBY_VERSION=$RUBY_VERSION --build-arg BUILD_OPTS=$2 .) \
+&& cat <<GUIDE
+# build finished successfuly.
 # If you push image to DockerHub, use below command:
 
 docker login
 
 docker push $NAME_TAG
+
 GUIDE
-fi
